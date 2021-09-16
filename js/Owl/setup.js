f099513f105ab9e391aadc243874672e0dd17443
W-JR <wiltonjsjunior@gmail.com>
2021-09-16 09:19:34 -0300

    first commit

---
+++ js/Owl/setup.js
@@ -0,0 +1,66 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+function buttonassistir(){
+    window.open("https://www.youtube.com/watch?v=Y_0088KD1_U");
+}
+function buttoninfo(){
+
+    window.open("https://www.omelete.com.br/filmes/criticas/critica-gran-torino");
+}
+
+function item1(){
+    
+    window.open("https://www.youtube.com/watch?v=zCzcZD65neE");
+    
+}
+
+function item2(){
+    window.open("https://www.youtube.com/watch?v=Wp5xUg8LKwY");
+    
+}
+
+function item3(){
+    window.open("https://www.youtube.com/watch?v=qyw94DV6LEk");
+    
+}
+
+function item4(){
+    window.open("https://www.youtube.com/watch?v=wL4a4MafSjQ");
+    
+}
+
+function item5(){
+    window.open("https://www.youtube.com/watch?v=GMKKq_bYd0E");
+    
+}
+
+function item6(){
+    window.open("https://www.youtube.com/watch?v=RiHkxUKe1XM");
+    
+}
+
+function item7(){
+    window.open("https://www.youtube.com/watch?v=0MJTCXX_LQUs");
+    
+}
+
+function item8(){
+    window.open("https://www.youtube.com/watch?v=xf8EZ_54OSw");
+    
+}
